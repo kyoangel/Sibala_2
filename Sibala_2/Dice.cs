093104a2ab81f4d627fa4883cafc17b1de8e4341
hatelove <jay.hatelove@gmail.com>
2017-08-23 00:15:34 +0800

    refactor

--- Sibala_2/Dice.cs
+++ Sibala_2/Dice.cs
@@ -62,8 +62,9 @@ namespace Sibala_2
                     this.Type = DiceType.Points;
                     if (diceGrouping.Count() == 2)
                     {
-                        this.Points = this._dices.Max() * 2;
-                        this.MaxPoint = this._dices.Max();
+                        var maxPoint = this._dices.Max();
+                        this.Points = maxPoint * 2;
+                        this.MaxPoint = maxPoint;
                     }
                     else
                     {
@@ -72,45 +73,13 @@ namespace Sibala_2
                         this.Points = dicesOfPoints.Sum();
                         this.MaxPoint = dicesOfPoints.Max();
                     }
-                    
+
                     break;
+
                 default:
                     this.Type = DiceType.NoPoint;
                     break;
-
             }
-            //switch (diceGrouping.Count())
-            //{
-            //    case 1:
-            //        this.Type = DiceType.Same;
-            //        this.Points = _dices.Sum();
-            //        this.MaxPoint = _dices.First();
-            //        break;
-
-            //    case 2:
-            //        if (diceGrouping.Max(x => x.Count()) == 3)
-            //        {
-            //            this.Type = DiceType.NoPoint;
-            //        }
-            //        else
-            //        {
-            //            this.Type = DiceType.Points;
-            //            this.Points = diceGrouping.Max(s => s.Key) * 2;
-            //            this.MaxPoint = _dices.Max();
-            //        }
-
-            //        break;
-
-            //    case 3:
-            //        this.Type = DiceType.Points;
-            //        this.Points = diceGrouping.Where(g => g.Count() < 2).Sum(s => s.Key);
-            //        this.MaxPoint = diceGrouping.Where(g => g.Count() == 1).Max(s => s.Key);
-            //        break;
-
-            //    default:
-            //        this.Type = DiceType.NoPoint;
-            //        break;
-            //}
         }
     }
 }
