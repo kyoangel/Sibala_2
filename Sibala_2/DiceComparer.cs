223e0237dc986cc3883543eb4ddd56248c0de821
hatelove <jay.hatelove@gmail.com>
2017-08-23 01:00:06 +0800

    refactor: layout

--- Sibala_2/DiceComparer.cs
+++ Sibala_2/DiceComparer.cs
@@ -11,6 +11,13 @@ namespace Sibala_2
 
     public class DiceComparer : Comparer<Dice>
     {
+        private static readonly Dictionary<DiceType, IComparer<Dice>> diceSameTypeComparers = new Dictionary<DiceType, IComparer<Dice>>()
+        {
+            {DiceType.Same, new SameResultComparer() },
+            {DiceType.Points, new PointsResultComparer() },
+            {DiceType.NoPoint, new NoPointResultComparer() },
+        };
+
         public override int Compare(Dice dice1, Dice dice2)
         {
             return dice1.Type == dice2.Type
@@ -23,21 +30,14 @@ namespace Sibala_2
             return (int)dice1.Type - (int)dice2.Type;
         }
 
-        private static int SameTypeCompare(Dice dice1, Dice dice2)
-        {
-            return GetComparer(dice1).Compare(dice1, dice2);
-        }
-
-        private static readonly Dictionary<DiceType, IComparer<Dice>> diceSameTypeComparers = new Dictionary<DiceType, IComparer<Dice>>()
-        {
-            {DiceType.Same, new SameResultComparer() },
-            {DiceType.Points, new PointsResultComparer() },
-            {DiceType.NoPoint, new NoPointResultComparer() },
-        };
-
         private static IComparer<Dice> GetComparer(Dice dice1)
         {
             return diceSameTypeComparers[dice1.Type];
+        }
+
+        private static int SameTypeCompare(Dice dice1, Dice dice2)
+        {
+            return GetComparer(dice1).Compare(dice1, dice2);
         }
     }
 }
