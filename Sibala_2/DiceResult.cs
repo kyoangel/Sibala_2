ab3b9cda88c47bb02615c7dd28029f92a313d4c3
xinyi <xinyi.kao@titansoft.com.sg>
2017-08-22 17:35:10 +0800

    init diceResult

---
+++ Sibala_2/DiceResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibala_2
+{
+    public enum DiceType {
+        Same,
+        Points,
+        NoPoint
+    }
+    public class DiceResult
+    {
+        int points { get; set; }
+        DiceType type { get; set; }
+
+    }
+}
---
+++ Sibala_2/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sibala_2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_Same_24_BiggerThan_Same_20()
+        {
+            //var diceResult = [6, 6, 6, 6];
+        }
+        
+    }
+}
