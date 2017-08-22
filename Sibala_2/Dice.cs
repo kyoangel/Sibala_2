03727f1f7333acd8d9fa8aad6d62c106befe4021
hatelove <jay.hatelove@gmail.com>
2017-08-23 00:13:41 +0800

    refactor: change calculate result algorithm

--- Sibala_2/Dice.cs
+++ Sibala_2/Dice.cs
@@ -49,38 +49,68 @@ namespace Sibala_2
         private void SetResult()
         {
             var diceGrouping = _dices.GroupBy(x => x);
-            switch (diceGrouping.Count())
+            var maxCountOfSameDice = diceGrouping.Max(x => x.Count());
+            switch (maxCountOfSameDice)
             {
-                case 1:
+                case 4:
                     this.Type = DiceType.Same;
-                    this.Points = _dices.Sum();
-                    this.MaxPoint = _dices.First();
+                    this.Points = this._dices.Sum();
+                    this.MaxPoint = this._dices.First();
                     break;
 
                 case 2:
-                    if (diceGrouping.Max(x => x.Count()) == 3)
+                    this.Type = DiceType.Points;
+                    if (diceGrouping.Count() == 2)
                     {
-                        this.Type = DiceType.NoPoint;
+                        this.Points = this._dices.Max() * 2;
+                        this.MaxPoint = this._dices.Max();
                     }
                     else
                     {
-                        this.Type = DiceType.Points;
-                        this.Points = diceGrouping.Max(s => s.Key) * 2;
-                        this.MaxPoint = _dices.Max();
+                        var duplicatePoint = diceGrouping.First(x => x.Count() == 2).Key;
+                        var dicesOfPoints = this._dices.Where(x => x != duplicatePoint);
+                        this.Points = dicesOfPoints.Sum();
+                        this.MaxPoint = dicesOfPoints.Max();
                     }
-
+                    
                     break;
-
-                case 3:
-                    this.Type = DiceType.Points;
-                    this.Points = diceGrouping.Where(g => g.Count() < 2).Sum(s => s.Key);
-                    this.MaxPoint = diceGrouping.Where(g => g.Count() == 1).Max(s => s.Key);
-                    break;
-
                 default:
                     this.Type = DiceType.NoPoint;
                     break;
+
             }
+            //switch (diceGrouping.Count())
+            //{
+            //    case 1:
+            //        this.Type = DiceType.Same;
+            //        this.Points = _dices.Sum();
+            //        this.MaxPoint = _dices.First();
+            //        break;
+
+            //    case 2:
+            //        if (diceGrouping.Max(x => x.Count()) == 3)
+            //        {
+            //            this.Type = DiceType.NoPoint;
+            //        }
+            //        else
+            //        {
+            //            this.Type = DiceType.Points;
+            //            this.Points = diceGrouping.Max(s => s.Key) * 2;
+            //            this.MaxPoint = _dices.Max();
+            //        }
+
+            //        break;
+
+            //    case 3:
+            //        this.Type = DiceType.Points;
+            //        this.Points = diceGrouping.Where(g => g.Count() < 2).Sum(s => s.Key);
+            //        this.MaxPoint = diceGrouping.Where(g => g.Count() == 1).Max(s => s.Key);
+            //        break;
+
+            //    default:
+            //        this.Type = DiceType.NoPoint;
+            //        break;
+            //}
         }
     }
 }
