793d395c4c38f125c8de375238f382fbbf696610
xinyi <xinyi.kao@titansoft.com.sg>
2017-08-22 18:28:56 +0800

    refactor test case

--- Sibala_2/UnitTest1.cs
+++ Sibala_2/UnitTest1.cs
@@ -9,35 +9,34 @@ namespace Sibala_2
         [TestMethod]
         public void Test_Same_24_BiggerThan_Same_20()
         {
-            Dice d1 = new Dice(new int[] { 6, 6, 6, 6 });
-            Dice d2 = new Dice(new int[] { 5, 5, 5, 5 });
-
-            DiceResult diceResult = new DiceResult();
-
-            int result = diceResult.Compare(d1, d2);
-            Assert.AreEqual(1, result);
+            CompareDice(new int[] { 6, 6, 6, 6 }, new int[] { 5, 5, 5, 5 }, 1);
         }
 
         [TestMethod]
         public void Test_Same_24_equals_Same_24()
         {
-            Dice d1 = new Dice(new int[] { 6, 6, 6, 6 });
-            Dice d2 = new Dice(new int[] { 6, 6, 6, 6 });
-            DiceResult diceResult = new DiceResult();
-        
-            int result = diceResult.Compare(d1, d2);
-            Assert.AreEqual(0, result);
+            CompareDice(new int[] { 6, 6, 6, 6 }, new int[] { 6, 6, 6, 6 }, 0);
         }
 
         [TestMethod]
         public void Test_Same_4_biggerthan_point_12()
         {
-            Dice d1 = new Dice(new int[] { 1, 1, 1, 1 });
-            Dice d2 = new Dice(new int[] { 5, 5, 6, 6 });
+            CompareDice(new int[] { 1, 1, 1, 1 }, new int[] { 5, 5, 6, 6 }, 1);
+        }
+
+        [TestMethod]
+        public void Test_point_10_equal_point_10()
+        {
+            CompareDice(new int[] { 1, 1, 5, 5 }, new int[] { 1, 1, 5, 5 }, 0);
+            
+        }
+
+        private void CompareDice(int[] d1, int[] d2, int expect)
+        {
             DiceResult diceResult = new DiceResult();
 
-            int result = diceResult.Compare(d1, d2);
-            Assert.AreEqual(1, result);
+            int result = diceResult.Compare(new Dice(d1), new Dice(d2));
+            Assert.AreEqual(expect, result);
         }
 
     }
