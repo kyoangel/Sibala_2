3d3a1feae3f9dace2df6a2985906c499d5bfc876
kyoangel <gd92x2@hotmail.com>
2017-09-16 14:43:29 +0800

    refactor, compare bigger return positive number, equals return 0, smaller return negative number

--- Sibala_2/DiceCompare.cs
+++ Sibala_2/DiceCompare.cs
@@ -19,28 +19,24 @@ namespace Sibala_2
             DiceResult result1 = dice1.GetResult();
             DiceResult result2 = dice2.GetResult();
 
-            if ((int)result1.type == (int)result2.type)
+            if (result1.type == result2.type)
             {
                 if (result1.type == DiceType.Same)
                 {
-                    var tem1 = PointConvert[result1.points / 4];
-                    var tem2 = PointConvert[result2.points / 4];
+                    var temp1 = PointConvert[result1.points / 4];
+                    var temp2 = PointConvert[result2.points / 4];
 
-                    return tem1 > tem2 ? 1 : tem1 == tem2 ? 0 : -1;
+                    return temp1 - temp2;
                 }
                 else if (result1.type == DiceType.Points)
                 {
                     if (result1.points == result2.points)
                     {
-                        return result1.maxPoint > result2.maxPoint ? 1 : result1.maxPoint == result2.maxPoint ? 0 : -1;
-                    }
-                    else if (result1.points > result2.points)
-                    {
-                        return 1;
+                        return result1.maxPoint - result2.maxPoint;
                     }
                     else
                     {
-                        return -1;
+                        return result1.points - result2.points;
                     }
                 }
                 else
@@ -48,12 +44,12 @@ namespace Sibala_2
                     return 0;
                 }
             }
-            else if ((int)result1.type > (int)result2.type)
+            else if (result1.type > result2.type)
             {
                 return 1;
             }
             else
-            {
+            {// type1 < type2
                 return -1;
             }
 
--- Sibala_2/SibalaCompareDiceTest.cs
+++ Sibala_2/SibalaCompareDiceTest.cs
@@ -10,53 +10,60 @@ namespace Sibala_2
     public class SibalaCompareDiceTest
     {
         [Test]
-        public void Test_Same_24_BiggerThan_Same_20()
+        public void Test_Same_1_BiggerThan_Same_4()
         {
-            CompareDice(new int[] { 6, 6, 6, 6 }, new int[] { 5, 5, 5, 5 }, 1);
+            FirstBiggerThanSecond(new int[] { 1, 1, 1, 1 }, new int[] { 4, 4, 4, 4 });
         }
 
         [Test]
         public void Test_Same_24_equals_Same_24()
         {
-            CompareDice(new int[] { 6, 6, 6, 6 }, new int[] { 6, 6, 6, 6 }, 0);
+            FirstEqualsToSecond(new int[] { 6, 6, 6, 6 }, new int[] { 6, 6, 6, 6 });
         }
 
         [Test]
         public void Test_Same_4_biggerthan_point_12()
         {
-            CompareDice(new int[] { 1, 1, 1, 1 }, new int[] { 5, 5, 6, 6 }, 1);
+            FirstBiggerThanSecond(new int[] { 1, 1, 1, 1 }, new int[] { 5, 5, 6, 6 });
         }
 
         [Test]
         public void Test_point_10_equal_point_10()
         {
-            CompareDice(new int[] { 1, 1, 5, 5 }, new int[] { 1, 1, 5, 5 }, 0);
+            FirstEqualsToSecond(new int[] { 1, 1, 5, 5 }, new int[] { 1, 1, 5, 5 });
         }
 
         [Test]
         public void Test_point_3_biggerthan_nopoint_0()
         {
-            CompareDice(new int[] { 3, 3, 1, 2 }, new int[] { 1, 2, 3, 4 }, 1);
+            FirstBiggerThanSecond(new int[] { 3, 3, 1, 2 }, new int[] { 1, 2, 3, 4 });
         }
 
 
         [TestMethod]
         public void Test_same_1_biggerthan_same_4()
         {
-            CompareDice(new int[] { 1, 1, 1, 1 }, new int[] { 4, 4, 4, 4 }, 1);
+            FirstBiggerThanSecond(new int[] { 1, 1, 1, 1 }, new int[] { 4, 4, 4, 4 });
         }
 
         [Test]
-        public void Test_points_6_biggerThan_points_6()
+        public void Test_No_points_6_equals_No_points_6()
         {
-            CompareDice(new int[] { 1, 1, 5, 1 }, new int[] { 4, 4, 4, 2 }, 0);
+            FirstEqualsToSecond(new int[] { 1, 1, 5, 1 }, new int[] { 4, 4, 4, 2 });
         }
 
-        private void CompareDice(int[] d1, int[] d2, int expect)
+        private void FirstBiggerThanSecond(int[] d1, int[] d2)
         {
             var helper = new DiceCompare();
             int result = helper.Compare(new Dice(d1), new Dice(d2));
-            Assert.AreEqual(expect, result);
+            Assert.IsTrue(result>0);
+        }
+
+        private void FirstEqualsToSecond(int[] d1, int[] d2)
+        {
+            var helper = new DiceCompare();
+            int result = helper.Compare(new Dice(d1), new Dice(d2));
+            Assert.IsTrue(result == 0);
         }
     }
 }
